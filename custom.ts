2c5ce2e47efe204e2119eb5d6043fcd5c493770b
sun-suns <155649987+sun-suns@users.noreply.github.com>
2024-05-28 22:27:38 +0900

    更新 pxt.json, main.blocks, custom.ts

---
+++ custom.ts
@@ -0,0 +1,227 @@
+
+
+enum CustomBlock {
+    //% blockIdentity="blocks.block" enumval=22 block="ラピスラズリブロック"
+    //% jres alias=LAPIS_LAZULI_BLOCK
+    LapisLazuliBlock = 22,
+    //% blockIdentity="blocks.block" enumval=41 block="金のブロック"
+    //% jres alias=GOLD_BLOCK
+    GoldBlock = 41,
+    //% blockIdentity="blocks.block" enumval=42 block="鉄のブロック"
+    //% jres alias=IRON_BLOCK
+    IronBlock = 42,
+    //% blockIdentity="blocks.block" enumval=57 block="ダイヤモンドブロック"
+    //% jres alias=DIAMOND_BLOCK
+    DiamondBlock = 57,
+    //% blockIdentity="blocks.block" enumval=133 block="エメラルドブロック"
+    //% jres alias=EMERALD_BLOCK
+    EmeraldBlock = 133,
+    //% blockIdentity="blocks.block" enumval=45 block="レンガブロック"
+    //% jres alias=BRICKS
+    Bricks = 45,
+    //% blockIdentity="blocks.block" enumval=46 block="TNT"
+    //% jres alias=TNT
+    TNT = 46,
+    //% blockIdentity="blocks.block" enumval=81 block="サボテン"
+    //% jres alias=CACTUS
+    Cactus = 81,
+    //% blockIdentity="blocks.block" enumval=86 block="カボチャ"
+    //% jres alias=PUMPKIN
+    Pumpkin = 86,
+    //% blockIdentity="blocks.block" enumval=20 block="ガラス"
+    //% jres alias=GLASS
+    Glass = 20,
+    //% blockIdentity="blocks.block" enumval=219 block="紫色の彩釉テラコッタ"
+    //% jres alias=PURPLE_GLAZED_TERRACOTTA
+    PurpleGlazedTerracotta = 219,
+    //% blockIdentity="blocks.block" enumval=220 block="白色の彩釉テラコッタ"
+    //% jres alias=WHITE_GLAZED_TERRACOTTA
+    WhiteGlazedTerracotta = 220,
+    //% blockIdentity="blocks.block" enumval=221 block="橙色の彩釉テラコッタ"
+    //% jres alias=ORANGE_GLAZED_TERRACOTTA
+    OrangeGlazedTerracotta = 221,
+    //% blockIdentity="blocks.block" enumval=222 block="赤紫色の彩釉テラコッタ"
+    //% jres alias=MAGENTA_GLAZED_TERRACOTTA
+    MagentaGlazedTerracotta = 222,
+    //% blockIdentity="blocks.block" enumval=223 block="空色の彩釉テラコッタ"
+    //% jres alias=LIGHT_BLUE_GLAZED_TERRACOTTA
+    LightBlueGlazedTerracotta = 223,
+    //% blockIdentity="blocks.block" enumval=224 block="黄色の彩釉テラコッタ"
+    //% jres alias=YELLOW_GLAZED_TERRACOTTA
+    YellowGlazedTerracotta = 224,
+    //% blockIdentity="blocks.block" enumval=225 block="黄緑色の彩釉テラコッタ"
+    //% jres alias=LIME_GLAZED_TERRACOTTA
+    LimeGlazedTerracotta = 225,
+    //% blockIdentity="blocks.block" enumval=226 block="桃色の彩釉テラコッタ"
+    //% jres alias=PINK_GLAZED_TERRACOTTA
+    PinkGlazedTerracotta = 226,
+    //% blockIdentity="blocks.block" enumval=227 block="灰色の彩釉テラコッタ"
+    //% jres alias=GRAY_GLAZED_TERRACOTTA
+    GrayGlazedTerracotta = 227,
+    //% blockIdentity="blocks.block" enumval=228 block="薄灰色の彩釉テラコッタ"
+    //% jres alias=LIGHT_GRAY_GLAZED_TERRACOTTA
+    LightGrayGlazedTerracotta = 228,
+    //% blockIdentity="blocks.block" enumval=229 block="青緑色の彩釉テラコッタ"
+    //% jres alias=CYAN_GLAZED_TERRACOTTA
+    CyanGlazedTerracotta = 229,
+    //% blockIdentity="blocks.block" enumval=231 block="青色の彩釉テラコッタ"
+    //% jres alias=BLUE_GLAZED_TERRACOTTA
+    BlueGlazedTerracotta = 231,
+    //% blockIdentity="blocks.block" enumval=232 block="茶色の彩釉テラコッタ"
+    //% jres alias=BROWN_GLAZED_TERRACOTTA
+    BrownGlazedTerracotta = 232,
+    //% blockIdentity="blocks.block" enumval=233 block="緑色の彩釉テラコッタ"
+    //% jres alias=GREEN_GLAZED_TERRACOTTA
+    GreenGlazedTerracotta = 233,
+    //% blockIdentity="blocks.block" enumval=234 block="赤色の彩釉テラコッタ"
+    //% jres alias=RED_GLAZED_TERRACOTTA
+    RedGlazedTerracotta = 234,
+    //% blockIdentity="blocks.block" enumval=235 block="黒色の彩釉テラコッタ"
+    //% jres alias=BLACK_GLAZED_TERRACOTTA
+    BlackGlazedTerracotta = 235,
+    //% blockIdentity="blocks.block" enumval=236 block="白色のコンクリート"
+    //% jres alias=WHITE_CONCRETE
+    WhiteConcrete = 236,
+    //% blockIdentity="blocks.block" enumval=65772 block="橙色のコンクリート"
+    //% jres alias=ORANGE_CONCRETE
+    OrangeConcrete = 65772,
+    //% blockIdentity="blocks.block" enumval=131308 block="赤紫色のコンクリート"
+    //% jres alias=MAGENTA_CONCRETE
+    MagentaConcrete = 131308,
+    //% blockIdentity="blocks.block" enumval=196844 block="空色のコンクリート"
+    //% jres alias=LIGHT_BLUE_CONCRETE
+    LightBlueConcrete = 196844,
+    //% blockIdentity="blocks.block" enumval=262380 block="黄色のコンクリート"
+    //% jres alias=YELLOW_CONCRETE
+    YellowConcrete = 262380,
+    //% blockIdentity="blocks.block" enumval=327916 block="黄緑色のコンクリート"
+    //% jres alias=LIME_CONCRETE
+    LimeConcrete = 327916,
+    //% blockIdentity="blocks.block" enumval=393452 block="桃色のコンクリート"
+    //% jres alias=PINK_CONCRETE
+    PinkConcrete = 393452,
+    //% blockIdentity="blocks.block" enumval=458988 block="灰色のコンクリート"
+    //% jres alias=GRAY_CONCRETE
+    GrayConcrete = 458988,
+    //% blockIdentity="blocks.block" enumval=524524 block="薄灰色のコンクリート"
+    //% jres alias=LIGHT_GRAY_CONCRETE
+    LightGrayConcrete = 524524,
+    //% blockIdentity="blocks.block" enumval=590060 block="青緑色のコンクリート"
+    //% jres alias=CYAN_CONCRETE
+    CyanConcrete = 590060,
+    //% blockIdentity="blocks.block" enumval=655596 block="紫色のコンクリート"
+    //% jres alias=PURPLE_CONCRETE
+    PurpleConcrete = 655596,
+    //% blockIdentity="blocks.block" enumval=721132 block="青色のコンクリート"
+    //% jres alias=BLUE_CONCRETE
+    BlueConcrete = 721132,
+    //% blockIdentity="blocks.block" enumval=786668 block="茶色のコンクリート"
+    //% jres alias=BROWN_CONCRETE
+    BrownConcrete = 786668,
+    //% blockIdentity="blocks.block" enumval=852204 block="緑色のコンクリート"
+    //% jres alias=GREEN_CONCRETE
+    GreenConcrete = 852204,
+    //% blockIdentity="blocks.block" enumval=917740 block="赤色のコンクリート"
+    //% jres alias=RED_CONCRETE
+    RedConcrete = 917740,
+    //% blockIdentity="blocks.block" enumval=983276 block="黒色のコンクリート"
+    //% jres alias=BLACK_CONCRETE
+    BlackConcrete = 983276,
+    //% blockIdentity="blocks.block" enumval=241 block="白色の色付きガラス"
+    //% jres alias=WHITE_STAINED_GLASS
+    WhiteStainedGlass = 241,
+    //% blockIdentity="blocks.block" enumval=65777 block="橙色の色付きガラス"
+    //% jres alias=ORANGE_STAINED_GLASS
+    OrangeStainedGlass = 65777,
+    //% blockIdentity="blocks.block" enumval=131313 block="赤紫色の色付きガラス"
+    //% jres alias=MAGENTA_STAINED_GLASS
+    MagentaStainedGlass = 131313,
+    //% blockIdentity="blocks.block" enumval=196849 block="空色の色付きガラス"
+    //% jres alias=LIGHT_BLUE_STAINED_GLASS
+    LightBlueStainedGlass = 196849,
+    //% blockIdentity="blocks.block" enumval=262385 block="黄色の色付きガラス"
+    //% jres alias=YELLOW_STAINED_GLASS
+    YellowStainedGlass = 262385,
+    //% blockIdentity="blocks.block" enumval=327921 block="黄緑色の色付きガラス"
+    //% jres alias=LIME_STAINED_GLASS
+    LimeStainedGlass = 327921,
+    //% blockIdentity="blocks.block" enumval=393457 block="桃色の色付きガラス"
+    //% jres alias=PINK_STAINED_GLASS
+    PinkStainedGlass = 393457,
+    //% blockIdentity="blocks.block" enumval=458993 block="灰色の色付きガラス"
+    //% jres alias=GRAY_STAINED_GLASS
+    GrayStainedGlass = 458993,
+    //% blockIdentity="blocks.block" enumval=524529 block="薄灰色の色付きガラス"
+    //% jres alias=LIGHT_GRAY_STAINED_GLASS
+    LightGrayStainedGlass = 524529,
+    //% blockIdentity="blocks.block" enumval=590065 block="青緑色の色付きガラス"
+    //% jres alias=CYAN_STAINED_GLASS
+    CyanStainedGlass = 590065,
+    //% blockIdentity="blocks.block" enumval=655601 block="紫色の色付きガラス"
+    //% jres alias=PURPLE_STAINED_GLASS
+    PurpleStainedGlass = 655601,
+    //% blockIdentity="blocks.block" enumval=721137 block="青色の色付きガラス"
+    //% jres alias=BLUE_STAINED_GLASS
+    BlueStainedGlass = 721137,
+    //% blockIdentity="blocks.block" enumval=786673 block="茶色の色付きガラス"
+    //% jres alias=BROWN_STAINED_GLASS
+    BrownStainedGlass = 786673,
+    //% blockIdentity="blocks.block" enumval=852209 block="緑色の色付きガラス"
+    //% jres alias=GREEN_STAINED_GLASS
+    GreenStainedGlass = 852209,
+    //% blockIdentity="blocks.block" enumval=917745 block="赤色の色付きガラス"
+    //% jres alias=RED_STAINED_GLASS
+    RedStainedGlass = 917745,
+    //% blockIdentity="blocks.block" enumval=983281 block="黒色の色付きガラス"
+    //% jres alias=BLACK_STAINED_GLASS
+    BlackStainedGlass = 983281,
+    //% blockIdentity="blocks.block" enumval=0 block="空気"
+    //% jres alias=AIR
+    Air = 0
+}
+
+//% weight=999 color=#c71585 icon="\uf1b3"
+//% block="体験（たいけん）"
+namespace lesson0 {
+
+    //% weight=90
+    //% blockId=customPlace
+    //% block="$block を $pos におく"
+    //% block.shadow=customBlock
+    //% pos.shadow="positionXY"
+    //% topblock topblockWeight=85
+    //% shim=blocks::placeAsync promise
+    export function customPlace(block: number, pos: Position): void {
+        blocks.place(block, pos);
+    }
+
+    //% weight=85
+    //% blockId=customBlock
+    //% block="$block"
+    //% block.fieldEditor="gridpicker"
+    //% block.fieldOptions.width=340
+    //% block.fieldOptions.columns=8
+    //% block.fieldOptions.tooltips=true
+    //% block.fieldOptions.tooltipsXOffset="20"
+    //% block.fieldOptions.tooltipsYOffset="-20"
+    //% block.fieldOptions.maxRows="8"
+    //% block.fieldOptions.hasSearchBar=true
+    //% block.fieldOptions.hideRect=true
+    export function customBlock(block: CustomBlock): number {
+        return block;
+    }
+
+    //% weight=80
+    //% blockId=positionXY
+    //% block="たいよう：$x うえ：$y"
+    //% inlineInputMode=inline
+    //% x.defl=0
+    //% y.defl=0
+    export function positionXY(x: number, y: number): Position {
+        return world(x, y, 0);
+    }
+
+
+
+
+}
